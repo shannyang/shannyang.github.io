b106b0a6387b6f45067b28f106f1394befc3b00c
shannyang <shannyang@Spot.home>
2015-08-01 23:45:43 -0400

    Fixed scrollspy!!!

--- custom.js
+++ custom.js
@@ -1,5 +1,7 @@
 $(function() {
 
+  $('body').scrollspy({target: "#main-nav"});
+
   $('#main-nav a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
